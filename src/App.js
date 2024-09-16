beaaa5177331191787642d47828ce311ffaa3385
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2024-09-16 23:06:10 +0200

    updated pages and added tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
 import './App.css'
+import './index.css'
 import ExerciseDetails from './pages/ExerciseDetails'
 import Home from './pages/Home'
-import Navbar from  './components/Navbar'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
-   <Box width="400px"  sx={{ width: {xl: '1488px'} }}  m='auto'>
-    <Navbar/>
-       <Routes>
-        <Route path="/" element={<Home/>}  />
-        <Route path="/exercise/:id" element={<ExerciseDetails/>}  />
-       </Routes>
-       <Footer/>
-   </Box>
+    <>
+      <Box width="400px" sx={{ width: { xl: '1488px' } }} m='auto'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        </Routes>
+      </Box>
+      {/* Footer is now outside the Box */}
+      <Footer />
+    </>
   )
 }
 
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography className='text-black' fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
       And Repeat
@@ -14,7 +14,7 @@ const HeroBanner = () => (
       Check out the most effective exercises personalized to you
     </Typography>
     <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    <a href="#exercises" className='bg-black' style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center',  padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
     </Stack>
     <Typography 
       fontWeight={600} 
